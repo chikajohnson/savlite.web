f544068caa4d8856ad452bb1c0762b70071418f6
johnson <johnson.iyida@netopng.com>
2020-07-08 03:47:54 +0100

    Updates

---
+++ precache-manifest.264d9999647c8e1cd326f2fdf8e444fd.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4bd837daa8b7bd515e2000e9593a5b",
+    "url": "/savlite.web/index.html"
+  },
+  {
+    "revision": "acd7481614d6d1acbeb2",
+    "url": "/savlite.web/static/css/10.8fc4efd3.chunk.css"
+  },
+  {
+    "revision": "73ce3776f17ca5b3696b",
+    "url": "/savlite.web/static/css/11.8d3a486f.chunk.css"
+  },
+  {
+    "revision": "25272497a41f7271486e",
+    "url": "/savlite.web/static/css/12.8d3a486f.chunk.css"
+  },
+  {
+    "revision": "da7f5b9f3892746a8bd2",
+    "url": "/savlite.web/static/css/13.c5bfa72f.chunk.css"
+  },
+  {
+    "revision": "99d431537eb56bb02da7",
+    "url": "/savlite.web/static/css/14.c5bfa72f.chunk.css"
+  },
+  {
+    "revision": "62c0ce9d424aea4b52dc",
+    "url": "/savlite.web/static/css/15.ff8a2b0c.chunk.css"
+  },
+  {
+    "revision": "f81168ec9429cafec403",
+    "url": "/savlite.web/static/css/16.ff8a2b0c.chunk.css"
+  },
+  {
+    "revision": "57c5c54642162e05a729",
+    "url": "/savlite.web/static/css/17.c5bfa72f.chunk.css"
+  },
+  {
+    "revision": "a2b99ff8e6b3d7054020",
+    "url": "/savlite.web/static/css/18.b3c298c4.chunk.css"
+  },
+  {
+    "revision": "94cf65152b751f38d926",
+    "url": "/savlite.web/static/css/19.c2fec359.chunk.css"
+  },
+  {
+    "revision": "4f2e08573045d913414c",
+    "url": "/savlite.web/static/css/20.c2fec359.chunk.css"
+  },
+  {
+    "revision": "269d08d07790c659aafe",
+    "url": "/savlite.web/static/css/21.c4acceb0.chunk.css"
+  },
+  {
+    "revision": "3cb23b87e22b1f14eded",
+    "url": "/savlite.web/static/css/22.c4acceb0.chunk.css"
+  },
+  {
+    "revision": "281c1eb23553f08520be",
+    "url": "/savlite.web/static/css/23.ff8a2b0c.chunk.css"
+  },
+  {
+    "revision": "d2ab8ad7ccd10d4b1a0b",
+    "url": "/savlite.web/static/css/24.ff8a2b0c.chunk.css"
+  },
+  {
+    "revision": "66103cce0d3ccf6d673d",
+    "url": "/savlite.web/static/css/25.ff8a2b0c.chunk.css"
+  },
+  {
+    "revision": "b83a9e4c0778154e65d0",
+    "url": "/savlite.web/static/css/26.c5bfa72f.chunk.css"
+  },
+  {
+    "revision": "9d12023c65510df1a484",
+    "url": "/savlite.web/static/css/27.c5bfa72f.chunk.css"
+  },
+  {
+    "revision": "c6b1c8d805133390d252",
+    "url": "/savlite.web/static/css/29.4ab604ff.chunk.css"
+  },
+  {
+    "revision": "08d3f649223db8c20114",
+    "url": "/savlite.web/static/css/3.c829125e.chunk.css"
+  },
+  {
+    "revision": "18c4f7017b968898bf5e",
+    "url": "/savlite.web/static/css/30.67b29b81.chunk.css"
+  },
+  {
+    "revision": "e068a3cd404a51ac6d07",
+    "url": "/savlite.web/static/css/4.19a6cfcd.chunk.css"
+  },
+  {
+    "revision": "97e9361dd53697a9f67f",
+    "url": "/savlite.web/static/css/5.cab5e88a.chunk.css"
+  },
+  {
+    "revision": "808bbb159f218570d559",
+    "url": "/savlite.web/static/css/6.cab5e88a.chunk.css"
+  },
+  {
+    "revision": "dfa0413c4b9856183b56",
+    "url": "/savlite.web/static/css/7.19a6cfcd.chunk.css"
+  },
+  {
+    "revision": "6a6e4581c2a59e4cf633",
+    "url": "/savlite.web/static/css/8.8d3a486f.chunk.css"
+  },
+  {
+    "revision": "86d7607880494454803e",
+    "url": "/savlite.web/static/css/9.cab5e88a.chunk.css"
+  },
+  {
+    "revision": "4582abb4129e9e0e2605",
+    "url": "/savlite.web/static/css/main.3636939e.chunk.css"
+  },
+  {
+    "revision": "75d677db180170f5dc69",
+    "url": "/savlite.web/static/js/0.3d3faf2d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/savlite.web/static/js/0.3d3faf2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd7481614d6d1acbeb2",
+    "url": "/savlite.web/static/js/10.f69e17d3.chunk.js"
+  },
+  {
+    "revision": "73ce3776f17ca5b3696b",
+    "url": "/savlite.web/static/js/11.a717874e.chunk.js"
+  },
+  {
+    "revision": "25272497a41f7271486e",
+    "url": "/savlite.web/static/js/12.3dbbdceb.chunk.js"
+  },
+  {
+    "revision": "da7f5b9f3892746a8bd2",
+    "url": "/savlite.web/static/js/13.ebdfed08.chunk.js"
+  },
+  {
+    "revision": "99d431537eb56bb02da7",
+    "url": "/savlite.web/static/js/14.617a7081.chunk.js"
+  },
+  {
+    "revision": "62c0ce9d424aea4b52dc",
+    "url": "/savlite.web/static/js/15.124eea50.chunk.js"
+  },
+  {
+    "revision": "f81168ec9429cafec403",
+    "url": "/savlite.web/static/js/16.afd23ee2.chunk.js"
+  },
+  {
+    "revision": "57c5c54642162e05a729",
+    "url": "/savlite.web/static/js/17.2b741009.chunk.js"
+  },
+  {
+    "revision": "a2b99ff8e6b3d7054020",
+    "url": "/savlite.web/static/js/18.fbc8981f.chunk.js"
+  },
+  {
+    "revision": "94cf65152b751f38d926",
+    "url": "/savlite.web/static/js/19.310cd888.chunk.js"
+  },
+  {
+    "revision": "4f2e08573045d913414c",
+    "url": "/savlite.web/static/js/20.2ce70dbc.chunk.js"
+  },
+  {
+    "revision": "269d08d07790c659aafe",
+    "url": "/savlite.web/static/js/21.df969b6c.chunk.js"
+  },
+  {
+    "revision": "3cb23b87e22b1f14eded",
+    "url": "/savlite.web/static/js/22.3529819c.chunk.js"
+  },
+  {
+    "revision": "281c1eb23553f08520be",
+    "url": "/savlite.web/static/js/23.a4e8964d.chunk.js"
+  },
+  {
+    "revision": "d2ab8ad7ccd10d4b1a0b",
+    "url": "/savlite.web/static/js/24.19f41d24.chunk.js"
+  },
+  {
+    "revision": "66103cce0d3ccf6d673d",
+    "url": "/savlite.web/static/js/25.8f443ac0.chunk.js"
+  },
+  {
+    "revision": "b83a9e4c0778154e65d0",
+    "url": "/savlite.web/static/js/26.80c34181.chunk.js"
+  },
+  {
+    "revision": "9d12023c65510df1a484",
+    "url": "/savlite.web/static/js/27.2f611f50.chunk.js"
+  },
+  {
+    "revision": "2a52544204ad8b98ced1",
+    "url": "/savlite.web/static/js/28.74fca298.chunk.js"
+  },
+  {
+    "revision": "51f53469d83492a60170b17ede6444ab",
+    "url": "/savlite.web/static/js/28.74fca298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b1c8d805133390d252",
+    "url": "/savlite.web/static/js/29.0d99b63d.chunk.js"
+  },
+  {
+    "revision": "08d3f649223db8c20114",
+    "url": "/savlite.web/static/js/3.f0438360.chunk.js"
+  },
+  {
+    "revision": "036e51fc0372a8105da54a6014c6c775",
+    "url": "/savlite.web/static/js/3.f0438360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c4f7017b968898bf5e",
+    "url": "/savlite.web/static/js/30.54fcefa6.chunk.js"
+  },
+  {
+    "revision": "f7ea168af7885e6d164c",
+    "url": "/savlite.web/static/js/31.75e86b52.chunk.js"
+  },
+  {
+    "revision": "6e21ee07707004ccd522",
+    "url": "/savlite.web/static/js/32.f710f0ac.chunk.js"
+  },
+  {
+    "revision": "e068a3cd404a51ac6d07",
+    "url": "/savlite.web/static/js/4.a198c842.chunk.js"
+  },
+  {
+    "revision": "97e9361dd53697a9f67f",
+    "url": "/savlite.web/static/js/5.2da821dc.chunk.js"
+  },
+  {
+    "revision": "808bbb159f218570d559",
+    "url": "/savlite.web/static/js/6.ef4aa4de.chunk.js"
+  },
+  {
+    "revision": "dfa0413c4b9856183b56",
+    "url": "/savlite.web/static/js/7.156ccb53.chunk.js"
+  },
+  {
+    "revision": "6a6e4581c2a59e4cf633",
+    "url": "/savlite.web/static/js/8.590d0a4a.chunk.js"
+  },
+  {
+    "revision": "86d7607880494454803e",
+    "url": "/savlite.web/static/js/9.f9f613ad.chunk.js"
+  },
+  {
+    "revision": "4582abb4129e9e0e2605",
+    "url": "/savlite.web/static/js/main.179c6b26.chunk.js"
+  },
+  {
+    "revision": "2e845edfcf748dc25a85",
+    "url": "/savlite.web/static/js/runtime-main.3bb4be5b.js"
+  }
+]);
---
+++ worker.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-restricted-globals */
+// self.addEventListener('install', (event) => {
+//   console.log('👷', 'install', event);
+//   self.skipWaiting();
+// });
+
+// self.addEventListener('activate', (event) => {
+//   console.log('👷', 'activate', event);
+//   return self.clients.claim();
+// });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(fetch(event.request));
+// });
+// let CACHE_NAME = 'Cashingames';
+// let urlsToCache = [
+//   '/'
+// ];
+
+// // Install a service worker
+// self.addEventListener('install', event => {
+//   //Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
+
+// // Cache and return requests
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
+
+// // Update a service worker
+// self.addEventListener('activate', event => {
+//   let cacheWhitelist = ['CashingGames'];
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cacheName => {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
